b2749e2bf74b3f3c8a565c77eb0dfe694226bd72
Oam Khatavkar <oam@Oams-Air.lan>
2024-04-28 21:24:27 -0500

    Pushing two unit tests for setting the game mode to easy or hard

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import GameModeSelectionScreen from './screens/gameModeSelection';
+import WebAssemblyWrapper from './screens/geoquest_wasm.js';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./screens/geoquest_wasm.js', () => {
+  return () => Promise.resolve({
+      _readCSV: jest.fn(() => 'pointer'),
+      UTF8ToString: jest.fn(() => JSON.stringify({ some: 'data' }))
+  });
+});
+
+global.window.globalState = {};
+
+describe('GameModeSelectionScreen', () => {
+    test('sets the game mode correctly to "easy" when the Easy Mode button is clicked', async () => {
+        const { getByText } = render(<GameModeSelectionScreen />);
+        
+        const easyButton = getByText('Easy Mode');
+        fireEvent.click(easyButton);
+
+        await waitFor(() => expect(window.globalState.gameMode).toBe('easy'));
+    });
+
+    test('sets the game mode correctly to "hard" when the Hard Mode button is clicked', async () => {
+        const { getByText } = render(<GameModeSelectionScreen />);
+        
+        const hardButton = getByText('Hard Mode');
+        fireEvent.click(hardButton);
+        await waitFor(() => expect(window.globalState.gameMode).toBe('hard'));
+    });
 });
